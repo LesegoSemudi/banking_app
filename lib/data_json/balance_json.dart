3207666c853c36129b86c0b47dd8da15584278c8
Lejay <lesego777@gmail.com>
2023-02-12 14:18:45 +0200

    update

---
+++ lib/data_json/balance_json.dart
@@ -0,0 +1,22 @@
+const List balanceLists = [
+  {
+    "amount": "324 921.06",
+    "currency" : "\R",
+    "description": "ZAR - Rand"
+  },
+  {
+    "amount": "18 199.24",
+    "currency" : "\$",
+    "description": "USD - Dollar"
+  },
+  {
+    "amount": "21 021.43",
+    "currency" : "€",
+    "description":"EUR - Euro",
+  },
+  {
+    "amount": "32 543.76",
+    "currency" : "£",
+    "description":"GBP - Pound sterling",
+  }
+];
---
+++ lib/data_json/card_json.dart
@@ -0,0 +1,34 @@
+import 'dart:ui';
+
+import 'package:banking_app/theme/color.dart';
+
+const List cardLists = [
+  {
+    "amount": "324 921.06",
+    "currency": "\R",
+    "card_number": "5436 5436 **** 6643",
+    "valid_date": "08/24",
+    "bg_color": Color(0xFF161616)
+  },
+  {
+    "amount": "18 199.24",
+    "currency": "\$",
+    "card_number": "5436 5436 **** 6643",
+    "valid_date": "08/24",
+    "bg_color": green
+  },
+  {
+    "amount": "21 021.43",
+    "currency": "€",
+    "card_number": "2267 9312 **** 9836",
+    "valid_date": "02/26",
+    "bg_color": Color(0xFFe2b04b)
+  },
+  {
+    "amount": "32 543.76",
+    "currency": "£",
+    "card_number": "2267 9312 **** 9836",
+    "valid_date": "01/30",
+    "bg_color": Color(0xFF161616)
+  }
+];
